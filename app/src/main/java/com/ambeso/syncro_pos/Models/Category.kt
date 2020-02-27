66e2eb2f8b7044d51d221872d7315aadfe81dae9
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-02-27 23:33:24 +0800

    simplify class constructor

---
+++ app/src/main/java/com/ambeso/syncro_pos/Models/Category.kt
@@ -0,0 +1,6 @@
+package com.ambeso.syncro_pos.Models
+
+class Category (
+    var uxid: String,
+    var name: String
+)
--- app/src/main/java/com/ambeso/syncro_pos/Models/Product.kt
+++ app/src/main/java/com/ambeso/syncro_pos/Models/Product.kt
@@ -1,44 +1,15 @@
 package com.ambeso.syncro_pos.Models
 
-class Product {
+class Product (
 
-//    var id: Int = 0
-    var type: String? = null
-    var category: String? = null
-    var uxid: String? = null
-    var code: String? = null
-    var name: String? = null
-    var unit: String? = null
-    var basePrice: Int = 0
-    var sellPrice: Int = 0
-    var stockAmount: Int = 0
+    var type: String,
+    var category: String,
+    var uxid: String?,
+    var code: String,
+    var name: String,
+    var unit: String?,
+    var basePrice: Int,
+    var sellPrice: Int,
+    var stockAmount: Int
 
-
-//    constructor(id: Int, type: String, category: String, uxid: String, code: String, name: String,
-//                unit: String, base_price: Int, sell_price: Int, stock: Int) {
-//        this.id = id
-//        this.type = type
-//        this.category = category
-//        this.uxid = uxid
-//        this.code = code
-//        this.name = name
-//        this.unit = unit
-//        this.basePrice = base_price
-//        this.sellPrice = sell_price
-//        this.stockAmount = stock
-//    }
-
-    constructor( type: String, category: String, uxid: String, code: String, name: String,
-                unit: String, base_price: Int, sell_price: Int, stock: Int) {
-
-        this.type = type
-        this.category = category
-        this.uxid = uxid
-        this.code = code
-        this.name = name
-        this.unit = unit
-        this.basePrice = base_price
-        this.sellPrice = sell_price
-        this.stockAmount = stock
-    }
-}
+)
